cae085d38f9ad8475443d7288c83d3483848f9ee
Brad Williams <braginteractive@gmail.com>
2014-12-01 17:17:23 -0600

    Edits to the Bootstrap menu so other markup can be added to the nav-bar collapse

--- header.php
+++ header.php
@@ -38,20 +38,20 @@
 			<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
     		</div>
 
-			 <?php
-	            wp_nav_menu( array(
-	                'menu'              => 'primary',
-	                'theme_location'    => 'primary',
-	                'depth'             => 2,
-	                'container'         => 'div',
-	                'container_class'   => 'collapse navbar-collapse',
-	        		'container_id'      => 'bs-example-navbar-collapse-1',
-	                'menu_class'        => 'nav navbar-nav',
-	                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-	                'walker'            => new wp_bootstrap_navwalker())
-	            );
-        	?>
-        	</div><!-- /.container-fluid -->
+    			<div class="navbar-collapse collapse" id="bs-example-navbar-collapse-1">
+				 <?php
+		            wp_nav_menu( array(
+		                'menu'              => 'primary',
+		                'theme_location'    => 'primary',
+		                'depth'             => 2,
+		                'container'         => false,
+		                'menu_class'        => 'nav navbar-nav',
+		                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+		                'walker'            => new wp_bootstrap_navwalker())
+		            );
+	        	?>
+        		</div> <!-- .navbar-collapse -->
+        	</div><!-- /.container -->
 		</nav><!-- .navbar .navbar-default -->
 	</header><!-- #masthead -->
 

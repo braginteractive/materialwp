096a91db0b931899f5319b1f2506b57d65fb3f22
Brad Williams <braginteractive@gmail.com>
2014-12-02 17:12:57 -0600

    target on footer link

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 			<div class="row">
 				<div class="col-lg-12">
 					<div class="site-info pull-right">
-						<?php printf( __( '%1$s by %2$s.', 'materialwp' ), 'MaterialWP', '<a href="http://braginteractive.com" rel="designer">Brad Williams</a>' ); ?>
+						<?php printf( __( '%1$s by %2$s.', 'materialwp' ), 'MaterialWP', '<a href="http://braginteractive.com" target="_blank" rel="designer">Brad Williams</a>' ); ?>
 					</div><!-- .site-info -->
 				</div> <!-- col-lg-12 -->
 			</div><!-- .row -->

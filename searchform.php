1d619ce21b69e49b43b39357519d5eb5f098f367
Brad Williams <braginteractive@gmail.com>
2014-12-02 13:55:32 -0600

    Added search form for custom material style input

---
+++ searchform.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Search Form Template
+ *
+ */
+?>
+
+<form method="get" class="form-search" action="<?php echo home_url( '/' ); ?>">
+	<div class="row">
+		<div class="col-lg-12">
+			<input type="text" class="form-control search-query floating-label" name="s" placeholder="<?php esc_attr_e('Search', 'materialwp'); ?>" />
+		</div>
+	</div>
+</form>

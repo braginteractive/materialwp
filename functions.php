05b98417860c1ea1a43d7dec30c0c499565a0ad6
Brad Williams <braginteractive@gmail.com>
2014-12-02 13:50:58 -0600

    Minor edits to the sidebar

--- functions.php
+++ functions.php
@@ -82,8 +82,8 @@ function materialwp_widgets_init() {
 		'description'   => '',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
 	) );
 }
 add_action( 'widgets_init', 'materialwp_widgets_init' );

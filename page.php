c5afda524a31255062fdbc3005c453ea0ff48b42
schafsberg <stefan_gimborn@web.de>
2015-08-25 18:19:10 +0200

    fixed footer issue on empty sidebar

--- page.php
+++ page.php
@@ -15,24 +15,28 @@ get_header(); ?>
 <div class="container">
 	<div class="row">
 
-	<div id="primary" class="col-md-8 col-lg-8">
-		<main id="main" class="site-main" role="main">
+		<div id="primary" class="col-md-8 col-lg-8">
+			<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+					<?php get_template_part( 'content', 'page' ); ?>
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
+					<?php
+						// If comments are open or we have at least one comment, load up the comment template
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
+					?>
 
-			<?php endwhile; // end of the loop. ?>
+				<?php endwhile; // end of the loop. ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			</main><!-- #main -->
+		</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+		<?php get_sidebar(); ?>
+
+	</div> <!-- .row -->
+</div> <!-- .container -->
+
 <?php get_footer(); ?>
